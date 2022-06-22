8b2bbd3f45ead63a8f70d38716b084b83ff02709
taebong20 <taebong3090@gmail.com>
2022-06-22 10:36:23 +0900

    modify

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,22 +1,26 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
-const Movie = ({ coverImg, title, genres, summary, id }) => {
+function Movie({ id, coverImg, title, year, summary, genres }) {
   return (
-    <div>
-      <img src={coverImg} alt={title} />
-      <h2>
-        <Link to={`/movie/${id}`}>{title}</Link>
-      </h2>
-      <ul>
-        {genres.map((gen) => (
-          <li key={gen}>{gen}</li>
-        ))}
-      </ul>
-      <p>{summary}</p>
+    <div className={styles.movie}>
+      <img src={coverImg} alt={title} className={styles.movie__img} />
+      <div>
+        <h2 className={styles.movie__title}>
+          <Link to={`/movie/${id}`}>{title}</Link>
+        </h2>
+        <h3 className={styles.movie__year}>{year}</h3>
+        <p>{summary.length > 235 ? `${summary.slice(0, 235)} ...` : summary}</p>
+        <ul className={styles.movie__genres}>
+          {genres.map((g) => (
+            <li key={g}>{g}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-};
+}
 
 Movie.propTypes = {
   id: PropTypes.number.isRequired,
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,31 +1,35 @@
-import Movie from "../components/Movie";
 import { useEffect, useState } from "react";
+import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
-const Home = () => {
-  const [loading, setLoding] = useState(true);
+function Home() {
+  const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const getMovies = async () => {
     const json = await (
       await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
       )
     ).json();
     setMovies(json.data.movies);
-    setLoding(false);
+    setLoading(false);
   };
   useEffect(() => {
     getMovies();
   }, []);
   return (
-    <div>
+    <div className={styles.container}>
       {loading ? (
-        <h1>Loading...</h1>
+        <div className={styles.loader}>
+          <span>Loading...</span>
+        </div>
       ) : (
-        <div>
+        <div className={styles.movies}>
           {movies.map((movie) => (
             <Movie
               key={movie.id}
               id={movie.id}
+              year={movie.year}
               coverImg={movie.medium_cover_image}
               title={movie.title}
               summary={movie.summary}
@@ -36,6 +40,5 @@ const Home = () => {
       )}
     </div>
   );
-};
-
+}
 export default Home;
